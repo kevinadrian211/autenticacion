0a6db26f3f795bf978dd0b681c7fde4a1b488991
VisioTransit <kecvinadrian211@gmail.com>
2024-06-18 23:41:57 -0500

    first commit

---
+++ src/main/kotlin/com/example/autenticacion/AutenticacionApplication.kt
@@ -0,0 +1,11 @@
+package com.example.autenticacion
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AutenticacionApplication
+
+fun main(args: Array<String>) {
+	runApplication<AutenticacionApplication>(*args)
+}
